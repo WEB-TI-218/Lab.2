2bbde739442aac6b82f574fb9fce66cc32760db3
Nazar <nazari.gentchev@isa.utm.md>
2023-05-16 08:49:09 +0300

    создал контролер для продукта и страничку

---
+++ eUseControl.Web1/Controllers/CarsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eUseControl.BusinessLogic.Interfaces;
+using eUseControl.Web1.ViewsModels;
+
+namespace eUseControl.Web1.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly IAllCars _allCars;
+        private readonly IAllCarsCategory _allCatigories;
+
+        public CarsController(IAllCars iAllCars, IAllCarsCategory iCarsCat)//// можно ли интерфейс или надо класс реализ интерф
+        {
+            _allCars = iAllCars;
+            _allCatigories = iCarsCat;
+        }
+
+        public ViewResult List()
+        {
+            CarsListViewModel obj = new CarsListViewModel();
+            obj.allCars = _allCars.Cars;
+            obj.currCategory = "Автомобили";
+            return View(obj);
+        }
+    }
+}
---
+++ eUseControl.Web1/ViewsModels/CarsListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using eUseControl.Web1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eUseControl.Web1.ViewsModels
+{
+    public class CarsListViewModel
+    {
+        public IEnumerable<Car> allCars { get; set; }
+        public string currCategory { get; set; }
+    }
+}
